d4fc422e70437359ff703e7ad5c3f7942e6507a3
Luizbc2 <luizotaviomello82@gmail.com>
2025-06-05 22:11:50 -0300

    home background changes

--- views/home.view.php
+++ views/home.view.php
@@ -1,9 +1,13 @@
 <main>
     <section id="home" class="hero-section d-flex align-items-center">
+        <video autoplay muted loop playsinline id="hero-video">
+            <source src="images/bolsas/capabolsas.mp4" type="video/mp4">
+            Seu navegador não suporta a tag de vídeo.
+        </video>
         <div class="container text-center">
             <h1 class="hero-title"><?= SITE_NAME ?></h1>
-            <p class="hero-subtitle">Bolsas artesanais de alto padrão com alma, amor e elegância.</p>
-            <a href="index.php?page=bolsas" class="btn btn-primary-custom mt-4">Descubra as Coleções</a>
+            <p class="hero-subtitle">Criações artesanais com alma, amor e elegância.</p>
+            <a href="bolsas" class="btn btn-primary-custom mt-4">Descubra as Coleções</a>
         </div>
     </section>
 </main>
